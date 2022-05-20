ce4fe73126c01c4f732ec4543e1f03785dd61e57
liztanyl <elizabeth.tanyulin@gmail.com>
2022-05-20 16:08:27 +0800

    render all climbs in a trip

--- src/components/Climbs/ClimbForm.js
+++ src/components/Climbs/ClimbForm.js
@@ -1,46 +1,52 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { BACKEND_URL } from "../../store.js";
+import { BACKEND_URL } from '../../store.js';
 
-export default function ClimbForm({ tripId }) {
-  const newClimb = { name: '', difficulty: 0 }
-  const [climbDetails, setClimbDetails] = useState(newClimb);
+export default function ClimbForm({ tripId, setClimbs }) {
+	const blankClimb = { name: '', difficulty: 0 };
+	const [climbDetails, setClimbDetails] = useState(blankClimb);
 
-  const handleChange = (e) => {
-    const { id, value } = e.target;
-    setClimbDetails({...climbDetails, [id]: value})
-  }
+	const handleChange = (e) => {
+		const { id, value } = e.target;
+		setClimbDetails({ ...climbDetails, [id]: value });
+	};
 
-  const handleSubmit = async () => {
-    const newClimbInfo = {
-      name: climbDetails.name,
-      difficulty: climbDetails.difficulty,
-      tripId
-    }
+	const handleSubmit = () => {
+		const newClimbInfo = {
+			name: climbDetails.name,
+			difficulty: climbDetails.difficulty,
+			tripId,
+		};
 
-    const newClimb = await axios.post(`${BACKEND_URL}/route/add`, newClimbInfo)
-    console.log(newClimb)
-  };
+		axios.post(`${BACKEND_URL}/route/add`, newClimbInfo).then((response) => {
+			const newClimb = response.data;
+			setClimbs((previousClimbs) => [...previousClimbs, newClimb]);
+		});
 
-  return (
-    <div>
-      <label for="name">Name of climb: </label>
-      <input
-        type="text"
-        placeholder="Enter a new climb location"
-        id="name"
-        value={climbDetails.name}
-        onChange={handleChange}
-      /><br/>
-      <label for="difficulty">Difficulty: </label>
-      <input
-        type="number"
-        id="difficulty"
-        value={climbDetails.difficulty}
-        min="0"
-        onChange={handleChange}
-      /><br />
-      <button onClick={handleSubmit}>Submit</button>
-    </div>
-  );
+		setClimbDetails(blankClimb);
+	};
+
+	return (
+		<div>
+			<label htmlFor="name">Name of climb: </label>
+			<input
+				type="text"
+				placeholder="Enter a new climb location"
+				id="name"
+				value={climbDetails.name}
+				onChange={handleChange}
+			/>
+			<br />
+			<label htmlFor="difficulty">Difficulty: </label>
+			<input
+				type="number"
+				id="difficulty"
+				value={climbDetails.difficulty}
+				min="0"
+				onChange={handleChange}
+			/>
+			<br />
+			<button onClick={handleSubmit}>Submit</button>
+		</div>
+	);
 }
